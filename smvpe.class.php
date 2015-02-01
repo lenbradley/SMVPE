84b039ae05df0d4f3e9dc3003c5bfbae7bc631ac
lenbradley <lenbradley@ninesphere.com>
2015-02-01 09:04:27 -0600

    Version 1.0.31 patch

--- smvpe.class.php
+++ smvpe.class.php
@@ -13,7 +13,7 @@
  * @license http://www.php.net/license/3_01.txt PHP License 3.01
  * @link    http://www.ninesphere.com
  * @package smvpe_package
- * @version 1.0.3
+ * @version 1.0.31
  * 
  */
 
